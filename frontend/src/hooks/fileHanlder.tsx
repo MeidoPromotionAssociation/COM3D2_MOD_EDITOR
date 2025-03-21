71f1db8298f1b6144b9855d5030d2d517dc529ee
90135 <193311049+90135@users.noreply.github.com>
2025-03-21 10:05:13 +0800

    fix wrong hook usage

--- frontend/src/hooks/fileHanlder.tsx
+++ frontend/src/hooks/fileHanlder.tsx
@@ -7,49 +7,6 @@ import {message} from "antd";
 // 支持的文件类型，用分号分隔
 export const AllSupportedFileTypes = "*.menu;*.mate;*.pmat;*.col;*.phy;.tex"
 
-
-const fileNavigateHandler = async (filePath: string) => {
-    const {t} = useTranslation();
-    const navigate = useNavigate();
-
-    const extension = getFileExtension(filePath);
-    switch (extension) {
-        case "menu":
-            navigate("/menu-editor", {state: {filePath}});
-            break;
-        case "mate":
-            navigate("/mate-editor", {state: {filePath}});
-            break;
-        case "pmat":
-            navigate("/pmat-editor", {state: {filePath}});
-            break;
-        case "col":
-            navigate("/col-editor", {state: {filePath}});
-            break;
-        case "phy":
-            navigate("/phy-editor", {state: {filePath}});
-            break;
-        case "tex":
-            navigate("/tex-editor", {state: {filePath}});
-            break;
-        default:
-            let isSupportedImage = false;
-            try {
-                isSupportedImage = await IsSupportedImageType(filePath)
-            } catch (err: any) {
-                message.error(t('Errors.file_type_not_supported') + ' ' + err);
-            }
-
-            if (isSupportedImage) {
-                navigate("/tex-editor", {state: {filePath}});
-            } else {
-                message.error(t('Errors.file_type_not_supported'));
-            }
-    }
-}
-
-
-
 // 1. handleSelectFile 选择文件，并转跳到对应页面
 // 2. handleSaveFile 保存文件，只是提示用户没有文件可以保存
 // 3. handleOpenedFile 处理已打开的文件，转跳到对应页面
@@ -57,6 +14,7 @@ const fileNavigateHandler = async (filePath: string) => {
 // description: 选择文件对话框的描述
 const useFileHandlers = () => {
     const {t} = useTranslation();
+    const navigate = useNavigate();
 
     const handleSelectFile = async (fileTypes: string, description: string) => {
         try {
@@ -73,6 +31,43 @@ const useFileHandlers = () => {
 
     const handleOpenedFile = async (filePath: string) => {
         await fileNavigateHandler(filePath)
+    }
+
+    const fileNavigateHandler = async (filePath: string) => {
+        const extension = getFileExtension(filePath);
+        switch (extension) {
+            case "menu":
+                navigate("/menu-editor", {state: {filePath}});
+                break;
+            case "mate":
+                navigate("/mate-editor", {state: {filePath}});
+                break;
+            case "pmat":
+                navigate("/pmat-editor", {state: {filePath}});
+                break;
+            case "col":
+                navigate("/col-editor", {state: {filePath}});
+                break;
+            case "phy":
+                navigate("/phy-editor", {state: {filePath}});
+                break;
+            case "tex":
+                navigate("/tex-editor", {state: {filePath}});
+                break;
+            default:
+                let isSupportedImage = false;
+                try {
+                    isSupportedImage = await IsSupportedImageType(filePath)
+                } catch (err: any) {
+                    message.error(t('Errors.file_type_not_supported') + ' ' + err);
+                }
+
+                if (isSupportedImage) {
+                    navigate("/tex-editor", {state: {filePath}});
+                } else {
+                    message.error(t('Errors.file_type_not_supported'));
+                }
+        }
     }
 
     return {handleSelectFile, handleSaveFile};
