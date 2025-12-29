1311cb0f1e1fb8ad2fb836759f2f7b9c989636f9
90135 <193311049+90135@users.noreply.github.com>
2025-12-29 16:50:35 +0800

    Sync Upstream

--- internal/service/COM3D2/preset.go
+++ internal/service/COM3D2/preset.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	"github.com/MeidoPromotionAssociation/MeidoSerialization/serialization/COM3D2"
+	"github.com/MeidoPromotionAssociation/MeidoSerialization/serialization/binaryio/stream"
 )
 
 // PresetService 专门处理 .preset 文件的读写
@@ -30,8 +31,8 @@ func (s *PresetService) ReadPresetFile(path string) (*COM3D2.Preset, error) {
 		return presetData, nil
 	}
 
-	br := bufio.NewReaderSize(f, 1024*64) //64KB 缓冲区，3231 个样本中 90% 文件小于 58.80 KB，中位数 50.57 KB，平均 51.32 KB
-	presetData, err := COM3D2.ReadPreset(br)
+	br := bufio.NewReaderSize(f, 1024*64)
+	presetData, err := COM3D2.ReadPreset(br) // 64KB 缓冲区，4574 个样本中 90% 文件小于 58.83 KB，平均 51.33 KB，中位数 50.51 KB，最大值 157.35 KB
 	if err != nil {
 		return nil, fmt.Errorf("parsing the .preset file failed: %w", err)
 	}
@@ -57,7 +58,8 @@ func (s *PresetService) ReadPresetFileMetadata(path string) (*COM3D2.PresetMetad
 	}
 
 	br := bufio.NewReaderSize(f, 1024*64) //64KB 缓冲区， 3231 个样本中 90% 文件小于 58.80 KB，中位数 50.57 KB
-	presetData, err := COM3D2.ReadPresetMetadata(br)
+	reader := stream.NewBinaryReader(br)
+	presetData, err := COM3D2.ReadPresetMetadata(reader)
 	if err != nil {
 		return nil, fmt.Errorf("parsing the .preset file failed: %w", err)
 	}
